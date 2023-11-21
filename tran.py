9b07834635c63d5f9f647cdb79981ad8d75c8ac1
thebruhFr <151537718+thebruhFr@users.noreply.github.com>
2023-11-21 17:12:53 +0630

    Update tran.py

--- tran.py
+++ tran.py
@@ -1,1 +1,8 @@
+from googletrans import Translator
 
+translator = Translator()
+
+# Translate from English to Spanish
+translation = translator.translate('broken heart!', dest='my')
+
+print(translation.text)
